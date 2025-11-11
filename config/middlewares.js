3d835f11a87a2fc204b9698b7035c09af9110db3
Arturo Pacheco <arturopsolis@gmail.com>
2025-11-11 09:19:29 -0600

    Configure CORS for knitboxing.corazolana.com subdomain

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,60 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'https://strapi-production-9c41.up.railway.app',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://strapi-production-9c41.up.railway.app',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        // Desarrollo local
+        'http://localhost:5174',
+        'http://localhost:1337',
+
+        // Railway (Strapi admin)
+        'https://strapi-production-9c41.up.railway.app',
+
+        // Producción - KnitBoxing
+        'https://knitboxing.corazolana.com',
+
+        // Por si acaso usas www
+        'https://www.knitboxing.corazolana.com',
+      ],
+      credentials: true,
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
